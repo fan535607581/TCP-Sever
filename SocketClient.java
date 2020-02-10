848b8d331bc5c420afdd56387e918a1e71d16992
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-10 15:13:48 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -7,6 +7,70 @@ import java.net.SocketException;
 
 import org.apache.commons.net.telnet.TelnetClient;
 
+@DesignerComponent(version = SocketUtil.VERSION,
+    description = "made in fan hao jie \n QQ:535607581",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true,
+    iconName = "images/extension.png")
+
+@SimpleObject(external = true)
+public class SocketUtil extends AndroidNonvisibleComponent {
+    public static final int VERSION = 1;//控件版本号
+    private static final String LOG_TAG = "SocketUtil";
+    private ComponentContainer container;
+    private Context context;
+    private ServerSocket serverSocket = null;
+	
+    String ip;//系统返回IP地址
+    int port;//系统返回端口
+    int con = 0;//控制信号
+    byte[] bb = new byte[1000];//回复数据
+    int[] i = new int[1000];//回复原始数据
+    int k = 0;//回复数据的长度
+    int DK = 0;//外部设置的端口
+		
+    public Handler handler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg){ GetMessage(msg.obj.toString()); }
+    };
+	
+    public SocketUtil(ComponentContainer container) 
+    {
+        super(container.$form());
+        this.container = container;
+        context = (Context) container.$context();
+    }
+    
+    @SimpleFunction(description = "start")//软件向控件写回复信息
+    public void sendMessage(String s)
+    {
+	 k = s.length()/3;
+	 for(int j = 0; j<k ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
+	 for(int j = 0; j<k+1 ;j++){bb[j+1] = (byte)i[j];} 
+	 //con=1;
+	 //new ServerThread2().start();
+    }
+    @SimpleFunction(description = "start")//断开客户端
+    public void Clientclose(){con = 2;}
+	
+    @SimpleFunction(description = "start")//关闭服务器
+    public void Serverclose(){try{serverSocket.close();}catch (IOException e) {}}
+	
+    @SimpleEvent//向软件输出信息
+    public void GetMessage(String s){ EventDispatcher.dispatchEvent(this, "GetMessage", s); }
+	
+    @SimpleFunction(description = "start")//打开通信端口
+    public void receiveData(int PORT){
+        
+        Thread thread = new Thread()//等待客户端连接的进程
+        {
+            @Override
+            public void run() { }
+        };
+        thread.start();
+ }
+    
 public class Main {
 
     public static void main(String[] args) {
