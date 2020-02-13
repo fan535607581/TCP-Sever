45c633b2d9241fcd8e33dd41c33edaa70d72148f
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-13 18:13:05 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -38,8 +38,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.io.PrintStream;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
