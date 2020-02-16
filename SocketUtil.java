d8ecb2532f86ca8fd28974285772142ae0e22100
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-16 21:13:32 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -63,8 +63,6 @@ public class SocketUtil extends AndroidNonvisibleComponent {
     int port;//系统返回端口
     int con = 0;//控制信号
     byte[] bb = new byte[1000];//回复数据
-    int[] i = new int[1000];//回复原始数据
-    int k = 0;//回复数据的长度
     int DK = 0;//外部设置的端口
 		
     public Handler handler = new Handler()
@@ -99,6 +97,9 @@ public class SocketUtil extends AndroidNonvisibleComponent {
     @SimpleFunction(description = "start")//软件向控件写回复信息
     public void sendMessage(String s)
     {
+	 int k = 0;//回复数据的长度
+	 int[] i = new int[1000];//回复原始数据
+	    
 	 k = s.length()/3;
 	 for(int j = 0; j<k ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
 	 for(int j = 0; j<k+1 ;j++){bb[j+1] = (byte)i[j];} 
