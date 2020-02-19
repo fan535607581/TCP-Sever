498561a484561393382895029a2245dd48402b1a
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-19 13:01:06 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -156,14 +156,15 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 		{
                     Socket socket = null;
                     try {
-                        socket = serverSocket.accept(); 
+                        socket = serverSocket.accept();
+			con=0;
+                        new ServerThread(socket).start();  
+			    
                         Message message_2 = handler.obtainMessage();
                         message_2.obj = "客户端连接："+socket.getInetAddress().getHostAddress();
                         handler.sendMessage(message_2);
                    	 } 
-		    catch (IOException e) {}
-			
-                    new ServerThread(socket).start();    
+		    catch (IOException e) {} 
                 }
             }
         };
